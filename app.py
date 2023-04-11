e0c355c120d9b2d0cb9c5e03306b2f8a437b9964
frncscp <104742131+frncscp@users.noreply.github.com>
2023-04-10 20:29:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title = 'PyUniban',
     page_icon = r"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
-    layout = 'centered'
+    layout = 'wide'
 )
 
 

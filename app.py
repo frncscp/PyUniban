4b7dbdd3dabb17c3900bfd7da8e1868b470cb66e
frncscp <104742131+frncscp@users.noreply.github.com>
2023-04-12 16:58:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from decimal import Decimal
 from math import e 
 
 def sigmoid(w, x, b): #pesos, entradas, sesgos
-    assert len(w) == len(x) == len(b), 'Las listas difieren en tamaño.'
+    assert len(w) == len(x) == len(b), 'Unequal lists\' shapes.'
     z = 0
     for weight, entrance, bias in zip(w, x, b):
         z+= weight*entrance-bias
